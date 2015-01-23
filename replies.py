3ef17881c25a92ea51539d5cbe597497e1b19f9a
Robert Weißgraeber <robert.weissgraeber@aexea.de>
2015-01-23 19:12:37 +0100

    Update replies.py

--- replies.py
+++ replies.py
@@ -5,6 +5,11 @@
 replies_dict = {
     'kommunist': ['Verdammte Kommunisten', 'Wer hat uns verraten?', 'Das ist nun doch zu einfach!'],
     'ak-47': ['Kommunisten-Waffe'],
+    'akbc': ['abcd'],
+    'akbc': ['abcd'],
+    'akbc': ['abcd'],
+    'akbc': ['abcd'],
+    'akbc': ['abcd'],'akbc': ['abcd'],
 }
 
 nothing_found = ['FIXME']
